43086e2876b390fbc2c4d6cd250b5905497a52db
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-08-31 15:06:54 -0700

    added extra comments for saga

--- app/sagas/sagas.js
+++ app/sagas/sagas.js
@@ -9,6 +9,7 @@ import {
   ADD_REQUEST_FAILED,
 } from 'constants/constants';
 
+// worker saga: makes the api call when watcher saga sees the GET_HOBBIES action
 function* getHobbiessAsync() {
   /**  
    * fetch hobbies from api
@@ -31,14 +32,15 @@ function* getHobbiessAsync() {
   }
   try {
     const hobbies = yield getHHobbies();
-    // dispatches GET_HOBBIES_ASYNC action if successfull
+
+    // dispatches GET_HOBBIES_ASYNC action to the store with hobbies
     yield put({ type: GET_HOBBIES_ASYNC, hobbies });
   } catch (error) {
-    // dispatches GET_REQUEST_FAILED action if failed
+    // dispatches GET_REQUEST_FAILED action to the store with the error
     yield put({ type: GET_REQUEST_FAILED, error });
   }
 }
-
+// worker saga: makes the api call when watcher saga sees the ADD_HOBBY action
 function* addHobbiessAsync(action) {
   // post a hobby to api
   /** 
@@ -64,18 +66,20 @@ function* addHobbiessAsync(action) {
   }
   try {
     const hobbies = yield addHobby();
-    // dispatches ADD_HOBBY_ASYNC action if successfull
+    // dispatches ADD_HOBBY_ASYNC action to the store with hobbies
     yield put({ type: ADD_HOBBY_ASYNC, hobbies });
   } catch (error) {
-    // dispatches ADD_REQUEST_FAILED action if failed
+    // dispatches ADD_REQUEST_FAILED action to the store with the error
     yield put({ type: ADD_REQUEST_FAILED, error });
   }
 }
-// listens for a dispatche to the Store that matches GET_HOBBIES pattern
+
+// watcher saga: watches for GET_HOBBIES action dispatched to the store, starts worker saga
 function* watchGetHobbiesAsync() {
   yield takeEvery(GET_HOBBIES, getHobbiessAsync);
 }
-// listens for a dispatche to the store that matches ADD_HOBBY pattern
+
+// watcher saga: watches for ADD_HOBBYaction dispatched to the store, starts worker saga
 function* watchAddHobbiesAsync() {
   yield takeEvery(ADD_HOBBY, addHobbiessAsync);
 }
