4162198a2b3171438e3f9ce96e2a108f84688f6d
Sam Merham <72219099+sammerham@users.noreply.github.com>
2021-09-01 06:48:14 -0700

    updated reducer

--- app/reducer.js
+++ app/reducer.js
@@ -1,15 +1,25 @@
-import { GET_HOBBIES_ASYNC, ADD_HOBBY_ASYNC } from './constants/constants';
+import {
+  GET_HOBBIES_ASYNC,
+  ADD_HOBBY_ASYNC,
+  GET_REQUEST_FAILED,
+  ADD_REQUEST_FAILED,
+} from './constants/constants';
 
 const DEFAULT_STATE = {
   hobbies: [],
+  error: false,
 };
 
 export default function hobbiesReducer(state = DEFAULT_STATE, action) {
   switch (action.type) {
     case GET_HOBBIES_ASYNC:
       return { hobbies: action.hobbies };
+    case GET_REQUEST_FAILED:
+      return { error: action.error };
     case ADD_HOBBY_ASYNC:
       return { hobbies: action.hobbies };
+    case ADD_REQUEST_FAILED:
+      return { error: action.error };
     default:
       return state;
   }
